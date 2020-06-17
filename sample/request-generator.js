76fe6594ddd405c5ba28c0bc5cad7aa511991340
Felipe Brizola <felipe_brizola@hotmail.com>
2020-06-17 18:06:40 -0300

    working with headers and body using http2

--- sample/request-generator.js
+++ sample/request-generator.js
@@ -13,12 +13,16 @@ const loadtest = require('../lib/loadtest.js');
 const options = {
 	url: 'https://localhost:3000/health',
 	concurrency: 5,
-	method: 'GET',
-	protocol: 'https',
+	method: 'POST',
+	protocol: 'http2',
 	insecure: true,
-	body:'',
-	requestsPerSecond:5,
-	maxSeconds:10
+	body: { a: 1 },
+	requestsPerSecond: 5,
+	maxSeconds: 10,
+	contentType: 'application/json',
+	headers: {
+		'x-seller-code': 123
+	}
 };
 
 loadtest.loadTest(options, (error, results) => {
