82ddcfbdd5e8ece536dc9db6f6eade12a8ca5ee3
Felipe Brizola <felipe_brizola@hotmail.com>
2020-06-17 14:28:59 -0300

    use http2 as protocol

--- sample/request-generator.js
+++ sample/request-generator.js
@@ -14,20 +14,11 @@ const options = {
 	url: 'https://localhost:3000/health',
 	concurrency: 5,
 	method: 'GET',
-	// body:'',
+	protocol: 'https',
+	insecure: true,
+	body:'',
 	requestsPerSecond:5,
-	maxSeconds:10,
-	
-	// requestGenerator: (params, options, client, callback) => {
-	// 	const message = '{"hi": "ho"}';
-	// 	options.headers['Content-Length'] = message.length;
-	// 	options.headers['Content-Type'] = 'application/json';
-	// 	options.body = 'YourPostData';
-	// 	options.path = 'YourURLPath';
-	// 	const request = client(options, callback);
-	// 	request.write(message);
-	// 	return request;
-	// }
+	maxSeconds:10
 };
 
 loadtest.loadTest(options, (error, results) => {
