ec7255c679582085730b17dfa8250964a54e7887
Felipe Brizola <felipe_brizola@hotmail.com>
2020-06-17 11:31:39 -0300

    http2 working

--- sample/request-generator.js
+++ sample/request-generator.js
@@ -8,23 +8,26 @@
 
 const loadtest = require('../lib/loadtest.js');
 
+// https://localhost:5003/api/receipt/cancellation/pos
+
 const options = {
-	url: 'http://yourHost',
+	url: 'https://localhost:3000/health',
 	concurrency: 5,
-	method: 'POST',
-	body:'',
+	method: 'GET',
+	// body:'',
 	requestsPerSecond:5,
-	maxSeconds:30,
-	requestGenerator: (params, options, client, callback) => {
-		const message = '{"hi": "ho"}';
-		options.headers['Content-Length'] = message.length;
-		options.headers['Content-Type'] = 'application/json';
-		options.body = 'YourPostData';
-		options.path = 'YourURLPath';
-		const request = client(options, callback);
-		request.write(message);
-		return request;
-	}
+	maxSeconds:10,
+	
+	// requestGenerator: (params, options, client, callback) => {
+	// 	const message = '{"hi": "ho"}';
+	// 	options.headers['Content-Length'] = message.length;
+	// 	options.headers['Content-Type'] = 'application/json';
+	// 	options.body = 'YourPostData';
+	// 	options.path = 'YourURLPath';
+	// 	const request = client(options, callback);
+	// 	request.write(message);
+	// 	return request;
+	// }
 };
 
 loadtest.loadTest(options, (error, results) => {
